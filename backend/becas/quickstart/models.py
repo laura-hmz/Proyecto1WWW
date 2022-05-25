e543c5748aa8c60ab1e2b2df8aa8d8ae1cd191b9
laura-hmz <laura.hernandez.munoz@correounivalle.edu.co>
2022-05-25 01:19:04 -0500

    Cambio en campos de bd
    Agrego más espacio a campos en bd

--- backend/becas/quickstart/models.py
+++ backend/becas/quickstart/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Beca(models.Model):
-    nombre=models.CharField(max_length=15)
-    categoria=models.CharField(max_length=15)
-    porcentaje_financiacion=models.IntegerField(default=20)
-    pais=models.CharField(max_length=15)
-    universidad=models.CharField(max_length=15)
-    requerimiento=models.CharField(max_length=100)
+    nombre=models.CharField(max_length=100)
+    categoria=models.CharField(max_length=100)
+    porcentaje_financiacion=models.IntegerField(default=100)
+    pais=models.CharField(max_length=100)
+    universidad=models.CharField(max_length=100)
+    requerimiento=models.CharField(max_length=600)
 
     def __str__(self):
         return self.nombre
