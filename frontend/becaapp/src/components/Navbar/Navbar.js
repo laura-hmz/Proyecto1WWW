d104e112794c0d6e76f534aa0422089e0270359a
laura-hmz <52580838+laura-hmz@users.noreply.github.com>
2022-05-27 19:11:56 -0500

    Navbar
    LA barra

---
+++ frontend/becaapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+//import {link} from 'react-router-dom';
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">HOME</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/"> Listado de becas </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/BecasForm"> Registrar nueva beca </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
